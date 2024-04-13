123aacc343172725bd0cfa14173470077bc1ba4c
RamonJales <ramonjales123@gmail.com>
2024-04-13 16:00:27 -0300

    interface componentes created

---
+++ impl/interface.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "../includes/interface.hpp"
+
+
+void print_menu() {
+    std::cout << "======================================" << std::endl;
+    std::cout << "           MENU DE OPÇÕES             " << std::endl;
+    std::cout << "======================================" << std::endl;
+    std::cout << "Selecione uma opção:" << std::endl;
+    std::cout << "1. Cadastrar Astronauta" << std::endl;
+    std::cout << "2. Cadastrar Voo" << std::endl;
+    std::cout << "3. Adicionar Astronauta em Voo" << std::endl;
+    std::cout << "4. Remover Astronauta do Voo" << std::endl;
+    std::cout << "5. Lançar Voo" << std::endl;
+    std::cout << "6. Explodir Voo" << std::endl;
+    std::cout << "7. Finalizar Voo" << std::endl;
+    std::cout << "8. Listar Todos os Voos" << std::endl;
+    std::cout << "9. Listar Todos os Astronautas mortos" << std::endl;
+    std::cout << "0. Sair" << std::endl;
+    std::cout << "======================================" << std::endl;
+    std::cout << "Escolha: ";
+}
--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 #include "includes/astronaut.hpp"
 #include "includes/flight.hpp"
+#include "includes/interface.hpp"
 #include <list>
 
 
 int main() {
+    std::list<Flight> databaseFlight;
+    std::list<Astronaut> databaseAstronaut;
 
-    Astronaut obj("ramon", "s11414551", 45);
-
-    std::list<Astronaut> list;
-
-    list.push_back(obj);
-
-    Flight flight1(1, list);
-
-    std::cout << "hello";
+    print_menu();
 
     return 0;
 }
