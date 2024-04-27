6b653a91faadb18bffd5a9c4b3677a201ee0ece3
RamonJales <ramonjales123@gmail.com>
2024-04-27 18:39:45 -0300

    available attribute added

--- impl/astronaut.cpp
+++ impl/astronaut.cpp
@@ -2,18 +2,21 @@
 
 Astronaut::Astronaut() {}
 
-Astronaut::Astronaut(int id, std::string cpf, std::string name, int age) {
+Astronaut::Astronaut(int id, std::string cpf, std::string name, int age, bool available) {
   this->id = id;
   this->name = name;
   this->cpf = cpf;
   this->age = age;
+  this->available = available;
 }
 
 int Astronaut::getId() { return this->id; }
 std::string Astronaut::getName() { return this->name; }
 std::string Astronaut::getCpf() { return this->cpf; }
 int Astronaut::getAge() { return this->age; }
+bool Astronaut::isAvailable() { return this->available; }
 
 void Astronaut::setName(std::string name) { this->name = name; }
 void Astronaut::setCpf(std::string cpf) { this->cpf = cpf; }
 void Astronaut::setAge(int age) { this->age = age; }
+void Astronaut::setAvailable(bool available) { this->available = available; }
--- includes/astronaut.hpp
+++ includes/astronaut.hpp
@@ -9,18 +9,21 @@ private:
   std::string name;
   std::string cpf;
   int age;
+  bool available;
 
 public:
   Astronaut();
-  Astronaut(int id, std::string cpf, std::string name, int age);
+  Astronaut(int id, std::string cpf, std::string name, int age, bool available);
 
   int getId();
   std::string getName();
   std::string getCpf();
   int getAge();
+  bool isAvailable();
 
   void setName(std::string name);
   void setCpf(std::string cpf);
   void setAge(int age);
+  void setAvailable(bool available);
 };
 #endif
