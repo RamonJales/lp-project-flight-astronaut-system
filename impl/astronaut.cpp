c35af4add748258764ae55affab70c7ea8981d1e
RamonJales <ramonjales123@gmail.com>
2024-04-27 19:07:03 -0300

    astronaut enum added in entity

--- impl/astronaut.cpp
+++ impl/astronaut.cpp
@@ -1,13 +1,15 @@
 #include "../includes/astronaut.hpp"
+#include "../includes/enums/AstronautStateEnum.hpp"
 
 Astronaut::Astronaut() {}
 
-Astronaut::Astronaut(int id, std::string cpf, std::string name, int age, bool available) {
+Astronaut::Astronaut(int id, std::string cpf, std::string name, int age, bool available, AstronautStateEnum astronautState) {
   this->id = id;
   this->name = name;
   this->cpf = cpf;
   this->age = age;
   this->available = available;
+  this->astronautState = astronautState;
 }
 
 int Astronaut::getId() { return this->id; }
@@ -15,8 +17,10 @@ std::string Astronaut::getName() { return this->name; }
 std::string Astronaut::getCpf() { return this->cpf; }
 int Astronaut::getAge() { return this->age; }
 bool Astronaut::isAvailable() { return this->available; }
+AstronautStateEnum Astronaut::getAstronautState() { return this->astronautState; }
 
 void Astronaut::setName(std::string name) { this->name = name; }
 void Astronaut::setCpf(std::string cpf) { this->cpf = cpf; }
 void Astronaut::setAge(int age) { this->age = age; }
 void Astronaut::setAvailable(bool available) { this->available = available; }
+void Astronaut::setAstronautState(AstronautStateEnum astronautState) { this->astronautState = astronautState; }
--- includes/astronaut.hpp
+++ includes/astronaut.hpp
@@ -2,6 +2,7 @@
 #define ASTRONAUT_HPP
 
 #include <string>
+#include "enums/AstronautStateEnum.hpp"
 
 class Astronaut {
 private:
@@ -10,20 +11,23 @@ private:
   std::string cpf;
   int age;
   bool available;
+  AstronautStateEnum astronautState;
 
 public:
   Astronaut();
-  Astronaut(int id, std::string cpf, std::string name, int age, bool available);
+  Astronaut(int id, std::string cpf, std::string name, int age, bool available, AstronautStateEnum astronautState);
 
   int getId();
   std::string getName();
   std::string getCpf();
   int getAge();
   bool isAvailable();
+  AstronautStateEnum getAstronautState();
 
   void setName(std::string name);
   void setCpf(std::string cpf);
   void setAge(int age);
   void setAvailable(bool available);
+  void setAstronautState(AstronautStateEnum astronautState);
 };
 #endif
